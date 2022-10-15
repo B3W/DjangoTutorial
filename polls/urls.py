8be71f331746cdcfbabd0fd429a24044a15eaf18
B3W <westieberg@msn.com>
2022-10-15 16:49:33 -0500

    + Built out the tutorial with more views. + Using templates for HTML + Admin can now access 'Question' table

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+# Namespace for this app's URLs
+app_name = 'polls'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),                                # /polls/
+    path('<int:question_id>/', views.detail, name='detail'),            # /polls/N/
+    path('<int:question_id>/results/', views.results, name='results'),  # /polls/N/results
+    path('<int:question_id>/vote/', views.vote, name='vote'),           # /polls/N/vote
 ]
