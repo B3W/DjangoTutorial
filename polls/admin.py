4f964c721e052d0602ce9d695bcdd011a683aba6
B3W <westieberg@msn.com>
2023-01-19 17:03:35 -0600

    + Customized the admin page to include Choice area when creating Question + Ignoring whitespace flake error code

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)  # Tells admin that Question objects have an admin interface
+
+# Allows Choice fields to be displayed when creating Question
+# Defines the number of default Choice fields when creating a Question
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+# Sets order of the fields on the admin page so 'question_text' is listed first
+# Adds a header over the published date via 'fieldsets'
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)  # Tells admin that Question objects have an admin interface
