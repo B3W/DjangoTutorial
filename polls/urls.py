a301a24395a7218ef325462d9387906b9909fa0b
B3W <westieberg@msn.com>
2023-01-19 15:32:48 -0600

    + Switched to using generic instead of custom views

--- polls/urls.py
+++ polls/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    path('', views.index, name='index'),                                # /polls/
-    path('<int:question_id>/', views.detail, name='detail'),            # /polls/N/
-    path('<int:question_id>/results/', views.results, name='results'),  # /polls/N/results
-    path('<int:question_id>/vote/', views.vote, name='vote'),           # /polls/N/vote
+    path('', views.IndexView.as_view(), name='index'),                       # /polls/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),            # /polls/N/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),  # /polls/N/results
+    path('<int:question_id>/vote/', views.vote, name='vote'),                # /polls/N/vote
 ]
